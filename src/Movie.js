d41f11af01e8ab1289aae432fc7cc776a4016cfe
minsangOh <flos9537@gmail.com>
2023-11-29 11:49:34 +0900

    github 배포

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,18 @@ import React from "react";
 import PropTypes from "prop-types"
 import "./Movie.css"
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return <div className="movie">
     <img src={poster} alt={title} title={title} />
     <div className="movie__data">
       <h3 className="movie__title"> {title} </h3>
       <h5 className="movie__year"> {year} </h5>
+      <ul className="movie__genres"> {genres.map((genre, index) => (
+        <li key={index} className="genres__gnere"> {genre} </li>
+      ))}
+      </ul>
       <h5 className="movie__summary"> {summary} </h5>
+      <hr />
     </div>
   </div>
 }
@@ -18,6 +23,7 @@ Movie.prototype = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
